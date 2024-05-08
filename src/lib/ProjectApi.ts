4ca29ee29f3bf9b1bbe180564339867b7a5fa6dd
Adrisa <adrisansyarif@gmail.com>
2024-05-08 20:59:54 +0700

    fix ProjectApi getProjectList and map projects in projects page

--- src/lib/ProjectApi.ts
+++ src/lib/ProjectApi.ts
@@ -1,40 +1,42 @@
-export const API_BASE = "https://groupbprojectbe-production.up.railway.app/"
-export const PROJECT_API = `${API_BASE}project/`
-export const PROJECT_API_DETAIL = `${PROJECT_API}id/`
+export const API_BASE = "https://groupbprojectbe-production.up.railway.app/";
+export const PROJECT_API = `${API_BASE}project/`;
+export const PROJECT_API_DETAIL = `${PROJECT_API}id/`;
 
 export interface Project {
-  id: number
-  project_name: string
-  project_image: string
-  description: string
-  target_amount: number
-  collected_amount: number
-  percentage: number
-  end_date: Date
+  id: number;
+  project_name: string;
+  project_image: string;
+  description: string;
+  target_amount: number;
+  collected_amount: number;
+  percentage: number;
+  end_date: Date;
 }
 
 export const getProjectList = async (): Promise<Project[]> => {
   try {
     const response = await fetch(`${API_BASE}project`);
 
-    if (!response.ok) {
-      throw new Error('Failed to fetch Project data');
+    if (response.status !== 200) {
+      throw new Error("Failed to fetch Project data");
     }
 
     const data = await response.json();
-    return data.results as Project[];
+    return data.projects as Project[];
   } catch (error) {
     // console.error('Error fetching Pokémon data:', (error as Error).message);
     throw error;
   }
 };
 
-export const getProjectDetails = async (projectName: string): Promise<Project | null> => {
+export const getProjectDetails = async (
+  projectName: string
+): Promise<Project | null> => {
   try {
     const response = await fetch(`${API_BASE}project/${projectName}`);
-    
+
     if (!response.ok) {
-      throw new Error('Failed to fetch Project details');
+      throw new Error("Failed to fetch Project details");
     }
 
     const data = await response.json();
@@ -46,7 +48,7 @@ export const getProjectDetails = async (projectName: string): Promise<Project | 
       collected_amount: data.collected_amount,
       target_amount: data.target_amount,
       percentage: data.percentage,
-      end_date: data.end_date
+      end_date: data.end_date,
     };
   } catch (error) {
     // console.error('Error fetching Pokémon details:', (error as Error).message);
--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -1,117 +1,39 @@
 import { Card, CardList, SearchField } from "@/components";
 import { useEffect, useState } from "react";
-import { getProjectList } from "@/lib/ProjectApi";
+import { getProjectList, Project } from "@/lib/ProjectApi";
 
 const ProjectsPage = () => {
-  // dummys array DATA
-
-  const projectsList = [
-    {
-      admin_id: 2,
-      description:
-        "test create project with admin test create project with admin  test create project with admin  test create project with admin  test create project with admin  test create project with admin  test create project with admin  test create project with admin  test create project with admin  test create project with admin  test create project with admin  test create project with admin  test create project with admin  test create project with admin  test create project with admin  test create project with admin  test create project with admin  test create project with admin  test create project with admin  test create project with admin  test create project with admin  test create project with admin  test create project with admin  test create project with admin  test create project with admin  test create project with admin ",
-      end_date: "Sun, 25 May 2025 00:00:00 GMT",
-      id: 5,
-      percentage: " 12.50%",
-      project_image:
-        "https://i.ibb.co/34d6Bnb/original-dd6082722592575153842caf2b0cdc40.png",
-      project_name: "testing lagi",
-      target_amount: "2000000.00",
-    },
-    {
-      admin_id: 2,
-      description: "test create project with admin",
-      end_date: "Sun, 25 May 2025 00:00:00 GMT",
-      id: 5,
-      percentage: " 12.50%",
-      project_image:
-        "https://i.ibb.co/34d6Bnb/original-dd6082722592575153842caf2b0cdc40.png",
-      project_name: "testing lagi",
-      target_amount: "2000000.00",
-    },
-    {
-      admin_id: 2,
-      description: "Bantuan Bencana Alam Gunung Erupsi",
-      end_date: "Sun, 25 May 2025 00:00:00 GMT",
-      id: 6,
-      percentage: " 3.00%",
-      project_image: "https://i.ibb.co/Y8DnG8q/Gunung-Api-Ruang-Meletus.jpg",
-      project_name: "Bantuan Gunung Erupsi",
-      target_amount: "20000000.00",
-    },
-    {
-      admin_id: 2,
-      description: "Bantuan Bencana Alam Gunung Erupsi",
-      end_date: "Sun, 25 May 2025 00:00:00 GMT",
-      id: 6,
-      percentage: " 3.00%",
-      project_image: "https://i.ibb.co/Y8DnG8q/Gunung-Api-Ruang-Meletus.jpg",
-      project_name: "Bantuan Gunung Erupsi",
-      target_amount: "20000000.00",
-    },
-    {
-      admin_id: 2,
-      description: "Bantuan Bencana Alam Gunung Erupsi",
-      end_date: "Sun, 25 May 2025 00:00:00 GMT",
-      id: 6,
-      percentage: " 3.00%",
-      project_image: "https://i.ibb.co/Y8DnG8q/Gunung-Api-Ruang-Meletus.jpg",
-      project_name: "Bantuan Gunung Erupsi",
-      target_amount: "20000000.00",
-    },
-    {
-      admin_id: 2,
-      description: "Bantuan Bencana Alam Gunung Erupsi",
-      end_date: "Sun, 25 May 2025 00:00:00 GMT",
-      id: 6,
-      percentage: " 3.00%",
-      project_image: "https://i.ibb.co/Y8DnG8q/Gunung-Api-Ruang-Meletus.jpg",
-      project_name: "Bantuan Gunung Erupsi",
-      target_amount: "20000000.00",
-    },
-    {
-      admin_id: 2,
-      description: "Bantuan Bencana Alam Gunung Erupsi",
-      end_date: "Sun, 25 May 2025 00:00:00 GMT",
-      id: 6,
-      percentage: " 3.00%",
-      project_image: "https://i.ibb.co/Y8DnG8q/Gunung-Api-Ruang-Meletus.jpg",
-      project_name: "Bantuan Gunung Erupsi",
-      target_amount: "20000000.00",
-    },
-    {
-      admin_id: 2,
-      description: "Bantuan Bencana Alam Gunung Erupsi",
-      end_date: "Sun, 25 May 2025 00:00:00 GMT",
-      id: 6,
-      percentage: " 3.00%",
-      project_image: "https://i.ibb.co/Y8DnG8q/Gunung-Api-Ruang-Meletus.jpg",
-      project_name: "Bantuan Gunung Erupsi",
-      target_amount: "20000000.00",
-    },
-  ];
-
   const [searchField, setSearchField] = useState("");
-  const [filteredProjects, setFilteredProjects] = useState(projectsList);
-  const [projects, setProjects] = useState([]);
+  const [filteredProjects, setFilteredProjects] = useState<Project[]>([]);
+  const [projects, setProjects] = useState<Project[]>([]);
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const searchFieldString = event.target.value.toLocaleLowerCase();
     setSearchField(searchFieldString);
   };
 
+  // console.log(getProject);
   useEffect(() => {
-    const get = getProjectList();
-    console.log(get);
+    const getProject = async () => {
+      const get = await getProjectList();
+      setProjects(get);
+    };
+    getProject();
   }, []);
 
   useEffect(() => {
-    const newFilteredProjects = projectsList.filter((project) => {
+    const newFilteredProjects = projects.filter((project) => {
       return project.project_name.toLocaleLowerCase().includes(searchField);
     });
 
     setFilteredProjects(newFilteredProjects);
   }, [projects, searchField]);
+
+  useEffect(() => {
+    const getDetailProject = async () => {
+      const;
+    };
+  }, []);
 
   return (
     <Card className="">
