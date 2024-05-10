3e24b09838510bbdd4f9657275fcc9f7cd93ad08
Hencimiru <hencimiru12@gmail.com>
2024-05-11 00:01:47 +0900

    about us page

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -1,7 +1,14 @@
+import { useState } from 'react';
 import Link from "next/link";
 import { Card, ListWrapper, TextTag } from "@/components";
 
 const Header: React.FC = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <Card id="header" className="flex flex-col md:flex-row items-center w-full pt-5 pb-5">
       <Card className="container flex items-center justify-between w-full px-4 mx-auto">
@@ -12,9 +19,27 @@ const Header: React.FC = () => {
           <Link href="/">LSTR!</Link>
         </TextTag>
 
-        <Card id="navbar" className="md:flex md:items-center">
+        <div id="navbar" className="md:flex md:items-center">
+          <button
+            onClick={toggleMenu}
+            className="block md:hidden focus:outline-none"
+          >
+            <svg
+              className="h-6 w-6 fill-current text-black"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+            >
+              <path
+                fillRule="evenodd"
+                d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"
+              />
+            </svg>
+          </button>
+
           <ListWrapper
-            className="flex flex-col md:flex-row md:space-x-4 md:space-y-0"
+            className={`${
+              isMenuOpen ? 'block' : 'hidden'
+            } md:flex md:flex-row md:space-x-4 md:space-y-0`}
             classNameChild="text-black hover:text-gray-700 px-3 py-2 rounded-md text-sm font-medium"
           >
             <Link href="/">Home</Link>
@@ -22,7 +47,7 @@ const Header: React.FC = () => {
             <Link href="/projects">Projects</Link>
             <Link href="/contact">Contact</Link>
           </ListWrapper>
-        </Card>
+        </div>
       </Card>
     </Card>
   );
